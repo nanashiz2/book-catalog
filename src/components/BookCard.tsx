6ed8bb65dcf7a4e691c2b558b0e109297ca3d226
nanashiz2 <santonu974@gmail.com>
2023-07-14 19:22:32 +0600

    added redux hook and store

---
+++ src/components/BookCard.tsx
@@ -0,0 +1,13 @@
+import { IBook } from "../types/globalTypes"
+
+interface Iprops {
+    books: IBook
+}
+
+function BookCard() {
+  return (
+    <div>BookCard</div>
+  )
+}
+
+export default BookCard
---
+++ src/types/globalTypes.ts
@@ -0,0 +1,9 @@
+export interface IBook {
+    _id: number
+    title: string
+    author: string
+    genre: string
+    publicationDate: string
+    reviews: string[]
+}
+
