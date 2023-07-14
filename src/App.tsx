88aaaf7eb43cbe9fe39393de783fb27d94b58740
nanashiz2 <santonu974@gmail.com>
2023-07-14 11:29:39 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='text-3xl font-bold underline'>hi</p>
+    </>
+  )
+}
+
+export default App
